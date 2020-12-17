8cad0ee330403b9c5429fa923a1103d68dc6142b
jcanete <joachimcanete@gmail.com>
2020-12-17 13:08:25 -0600

    attempting eventlister on TL buttonOne

--- Simon/script.js
+++ Simon/script.js
@@ -4,30 +4,40 @@ console.log("Simon says, 'STFU'")
 <div class="option" id="buttonTwo"> // TOP RIGHT - (Blue)
 <div class="option" id="buttonThree"> // BOTTOM LEFT - (Green)
 <div class="option" id="buttonFour"> // BOTTOM RIGHT - (Yellow)
+
+button background defuault rgb(235, 235, 235)
+
 */
 
-//on click, flash buttonOne to color (Red)
+// *------= Top Left Button BLINK(?) Red =------*
 
-const topLeft = document.querySelector("#buttonOne")
+const topLeft = document.querySelector(".option")
 topLeft.addEventListener("click", (event) => {
     event.preventDefault()
     console.log("you clicked TL")
+    topLeft.style.background.transition = "red"
+    
+    // topLeft.style.background = "initial"
+    //upon click, set color to default
 })
 
-const topRight = document.querySelector("#buttonTwo")
-topRight.addEventListener("click", (event) => {
-    event.preventDefault()
-    console.log("you clicked TR")
-})
+// const topRight = document.querySelector("#buttonTwo")
+// topRight.addEventListener("click", (event) => {
+//     event.preventDefault()
+//     console.log("you clicked TR")
+//     topRight.style.background = "blue"
+// })
 
-const botLeft = document.querySelector("#buttonThree")
-botLeft.addEventListener("click", (event) => {
-    event.preventDefault()
-    console.log("you clicked BL")
-})
+// const botLeft = document.querySelector("#buttonThree")
+// botLeft.addEventListener("click", (event) => {
+//     event.preventDefault()
+//     console.log("you clicked BL")
+//     botLeft.style.background = "green"
+// })
 
-const botRight = document.querySelector("#buttonFour")
-botRight.addEventListener("click", (event) => {
-    event.preventDefault()
-    console.log("you clicked BR")
-})
+// const botRight = document.querySelector("#buttonFour")
+// botRight.addEventListener("click", (event) => {
+//     event.preventDefault()
+//     console.log("you clicked BR")
+//     botRight.style.background = "yellow"
+// })
