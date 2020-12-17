c9d7542031b16b6832e290dccca96f40d8cfa393
jcanete <joachimcanete@gmail.com>
2020-12-17 11:15:48 -0600

    repeated code over four buttons

--- Simon/script.js
+++ Simon/script.js
@@ -1,7 +1,33 @@
 console.log("Simon says, 'STFU'")
+/*
+<div class="option" id="buttonOne"> // TOP LEFT - (Red)
+<div class="option" id="buttonTwo"> // TOP RIGHT - (Blue)
+<div class="option" id="buttonThree"> // BOTTOM LEFT - (Green)
+<div class="option" id="buttonFour"> // BOTTOM RIGHT - (Yellow)
+*/
 
-let buttonOne = document.querySelector("buttonOne")
-// buttonOne.addEventListener("click", (event) => {
-//     event.preventDefault()
-//     buttonOne.style.background = "red"
-// })
+//on click, flash buttonOne to color (Red)
+
+const topLeft = document.querySelector("#buttonOne")
+topLeft.addEventListener("click", (event) => {
+    event.preventDefault()
+    console.log("you clicked TL")
+})
+
+const topRight = document.querySelector("#buttonTwo")
+topRight.addEventListener("click", (event) => {
+    event.preventDefault()
+    console.log("you clicked TR")
+})
+
+const botLeft = document.querySelector("#buttonThree")
+botLeft.addEventListener("click", (event) => {
+    event.preventDefault()
+    console.log("you clicked BL")
+})
+
+const botRight = document.querySelector("#buttonFour")
+botRight.addEventListener("click", (event) => {
+    event.preventDefault()
+    console.log("you clicked BR")
+})
