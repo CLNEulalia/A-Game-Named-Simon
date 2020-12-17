11d987c833f2a8e1bddc3e11040d4d495c73047b
jcanete <joachimcanete@gmail.com>
2020-12-17 10:58:39 -0600

    cleaned up button layout. lade framework for button functionality

---
+++ Simon/script.js
@@ -0,0 +1,7 @@
+console.log("Simon says, 'STFU'")
+
+let buttonOne = document.querySelector("buttonOne")
+// buttonOne.addEventListener("click", (event) => {
+//     event.preventDefault()
+//     buttonOne.style.background = "red"
+// })
---
+++ Simon/testing.js
@@ -0,0 +1,24 @@
+<div>
+        <div class="option" id="buttonOne"><h2>1</h2></div>
+        <div class="option" id="buttonTwo"><h2>2</h2></div>
+
+        <div class="option" id="buttonThree"><h2>3</h2></div>
+        <div class="option" id="buttonFour"><h2>4</h2></div>
+</div>
+
+// *------= ORIGINAL =------*
+/*
+<div>
+        <table style="width:0%" id="theBoard">
+            <tr>
+                <td><div class="option" id="buttonOne"><h2>1</h2></div></td>
+                <td><div class="option" id="buttonTwo"><h2>2</h2></div></td>
+            </tr>
+
+            <tr>
+                <td><div class="option" id="buttonThree"><h2>3</h2></div></td>
+                <td><div class="option" id="buttonFour"><h2>4</h2></div></td>
+            </tr>
+        </table>
+    </div>
+*/
