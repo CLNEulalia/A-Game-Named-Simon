8d15ae180af4d7ba277f3d0929310f25cab4a0e6
jcanete <joachimcanete@gmail.com>
2020-12-17 14:38:50 -0600

    preittier button press effect

--- Simon/script.js
+++ Simon/script.js
@@ -1,4 +1,4 @@
-console.log("Simon says, 'STFU'")
+console.log("Swiggity swooty, Simon's on duty")
 /*
 <div class="option" id="buttonOne"> // TOP LEFT - (Red)
 <div class="option" id="buttonTwo"> // TOP RIGHT - (Blue)
@@ -15,29 +15,23 @@ const topLeft = document.querySelector("#buttonOne")
 topLeft.addEventListener("click", (event) => {
     event.preventDefault()
     console.log("you clicked TL")
-    topLeft.style.background = "red"
-    // topLeft.classList.toggle = "active"    
-    // topLeft.style.background = "initial"
-    //upon click, set color to default
+
 })
 
 const topRight = document.querySelector("#buttonTwo")
 topRight.addEventListener("click", (event) => {
     event.preventDefault()
     console.log("you clicked TR")
-    topRight.style.background = "blue"
 })
 
 const botLeft = document.querySelector("#buttonThree")
 botLeft.addEventListener("click", (event) => {
     event.preventDefault()
     console.log("you clicked BL")
-    botLeft.style.background = "green"
 })
 
 const botRight = document.querySelector("#buttonFour")
 botRight.addEventListener("click", (event) => {
     event.preventDefault()
     console.log("you clicked BR")
-    botRight.style.background = "yellow"
 })
